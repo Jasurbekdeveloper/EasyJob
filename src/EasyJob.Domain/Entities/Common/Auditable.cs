b205ca2857955d6a48e1abf4dcf65aeefdd86fbb
KhAbdukhoshimov <Khangitlab2710>
2023-01-09 12:12:52 +0500

    Added auditable entity

---
+++ src/EasyJob.Domain/Entities/Common/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyJob.Domain.Entities.Common;
+
+public abstract class Auditable
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
