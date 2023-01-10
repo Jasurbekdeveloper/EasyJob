2030f3a9c564201e1592a905a7d1396259311493
KhAbdukhoshimov <Khangitlab2710>
2023-01-10 12:32:09 +0500

    Implemented user service

---
+++ src/EasyJob.Application/Services/Users/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using EasyJob.Domain.Entities.Users;
+
+namespace EasyJob.Application.Services.Users;
+
+public interface IUserService
+{
+    ValueTask<User> CreateUserAsync(User user);
+    IQueryable<User> RetrieveUsers();
+    ValueTask<User> RetrieveUserByIdAsync(Guid userId);
+    ValueTask<User> ModifyUserAsync(User user);
+    ValueTask<User> RemoveUserAsync(Guid user);
+}
---
+++ src/EasyJob.Application/Services/Users/UserService.cs
@@ -0,0 +1,50 @@
+﻿using EasyJob.Domain.Entities.Users;
+using EasyJob.Infrastructure.Repositories.Users;
+
+namespace EasyJob.Application.Services.Users;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public async ValueTask<User> CreateUserAsync(User user)
+    {
+        var addedUser = await this.userRepository
+            .InsertAsync(user);
+
+        return addedUser;
+    }
+
+    public IQueryable<User> RetrieveUsers() =>
+        this.userRepository.SelectAll();
+
+    public async ValueTask<User> RetrieveUserByIdAsync(Guid userId)
+    {
+        return await this.userRepository
+            .SelectByIdAsync(userId);
+    }
+
+    public async ValueTask<User> ModifyUserAsync(User user)
+    {
+        var modifiedUser = await this.userRepository
+            .UpdateAsync(user);
+
+        return modifiedUser;
+    }
+
+    public async ValueTask<User> RemoveUserAsync(Guid userId)
+    {
+        var user = await this.userRepository
+            .SelectByIdAsync(userId);
+
+        var removedUser = await this.userRepository
+            .DeleteAsync(user);
+
+        return removedUser;
+    }
+}
