c621c921ef00116d3cafcd703574e31bf9c88c0b
KhAbdukhoshimov <Khangitlab2710>
2023-01-10 10:41:17 +0500

    Implemented Generic Repository

---
+++ src/EasyJob.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using EasyJob.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyJob.Infrastructure.Repositories;
+
+public abstract class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey>
+    where TEntity : class
+{
+    private readonly AppDbContext appDbContext;
+
+    public GenericRepository(AppDbContext appDbContext)
+    {
+        this.appDbContext = appDbContext;
+    }
+
+    public async ValueTask<TEntity> InsertAsync(
+        TEntity entity)
+    {
+        var entityEntry = await this.appDbContext
+            .AddAsync<TEntity>(entity);
+
+        await this.SaveChangesAsync();
+
+        return entityEntry.Entity;
+    }
+
+    public IQueryable<TEntity> SelectAll() =>
+        this.appDbContext.Set<TEntity>();
+
+    public async ValueTask<TEntity> SelectByIdAsync(TKey id) =>
+        await this.appDbContext.Set<TEntity>().FindAsync(id);
+
+    public async ValueTask<TEntity> UpdateAsync(TEntity entity)
+    {
+        var entityEntry = this.appDbContext
+            .Update<TEntity>(entity);
+
+        await this.SaveChangesAsync();
+
+        return entityEntry.Entity;
+    }
+
+    public async ValueTask<TEntity> DeleteAsync(TEntity entity)
+    {
+        var entityEntry = this.appDbContext
+            .Set<TEntity>()
+            .Remove(entity);
+
+        await this.SaveChangesAsync();
+
+        return entityEntry.Entity;
+    }
+
+
+    public async ValueTask<int> SaveChangesAsync()
+    {
+        return await this.appDbContext
+            .SaveChangesAsync();
+    }
+}
---
+++ src/EasyJob.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyJob.Infrastructure.Repositories;
+
+public interface IGenericRepository<TEntity, TKey>
+{
+    ValueTask<TEntity> InsertAsync(TEntity entity);
+    IQueryable<TEntity> SelectAll();
+    ValueTask<TEntity> SelectByIdAsync(TKey id);
+    ValueTask<TEntity> UpdateAsync(TEntity entity);
+    ValueTask<TEntity> DeleteAsync(TEntity entity);
+    ValueTask<int> SaveChangesAsync();
+}
